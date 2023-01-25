671a375ce1b9804f1ad3f548b3711fd00d84afef
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-25 15:38:15 +0100

    Fix: - inserita nella classe Prodotto la variabile name; Sviluppo: -Creata nelle varie classi la funzione getNomeClasse per stampare a schermo l'informazione;

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 
 // creo la classe prodotti 
 class Prodotto {
+    private $name;
+    private Categoria $categoria;
+    private Tipologia $tipologia;
 
-    private $categoria;
-    private $tipologia;
-
-    public function __construct($categoria, $tipologia){
+    public function __construct( $name, Categoria $categoria, Tipologia $tipologia){
+        $this->name = $name;
         $this->categoria = $categoria;
         $this->tipologia = $tipologia;
     }
 
     public function getProdotto() {
-
+        return "<div>" . "<h1>" . $this -> name . "</h1>" 
+            . $this -> categoria -> getCategory()
+            . $this -> tipologia -> getTipologia()
+        . "</div>"; 
     }
 
 }
@@ -25,6 +29,10 @@ class Categoria extends Prodotto {
     public function __construct($categoria){
         $this->categoria = $categoria;
     }
+
+    public function getCategory(){
+        return "<span>" . $this->categoria . "</span>" . "<br>";
+    }
 }
 
 // creo la classe Tipologia
@@ -34,4 +42,15 @@ class Tipologia extends Prodotto {
     public function __construct($tipologia){
         $this->tipologia = $tipologia;
     }
+
+    public function getTipologia(){
+        return "<span>" . $this->tipologia . "</span>";
+    }
 }
+
+
+// creo i vari elementi da stampare successivamente
+// creo le categorie principali
+$dog = new Categoria("Cane");
+$cat = new Categoria("Gatto");
+
